94ffc9dbfd554932543864838ef8e3aee135f72b
Kishore <kishore22705@gmail.com>
2026-01-12 12:05:51 +0530

    Serving the HTML

--- api/index.py
+++ api/index.py
@@ -1,10 +1,18 @@
 from http.server import BaseHTTPRequestHandler
+import os
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+        try:
+            with open("index.html", "rb") as f:
+                data = f.read()
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.send_header("Content-Length", str(len(data)))
+            self.end_headers()
+            self.wfile.write(data)
+        except FileNotFoundError:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
